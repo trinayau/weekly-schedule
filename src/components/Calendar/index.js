cbcde6cd03870161c5dcf49b0d4af5efa7deac5d
Trina <92634994+trinayau@users.noreply.github.com>
2023-01-31 13:32:21 +0000

    add initial styling for calendar using react-bootstrap

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,7 +1,49 @@
+import {Table, Container, Row, Col} from 'react-bootstrap';
+
 const Calendar = () => {
-    return ( <>
-    hi
-    </> );
-}
- 
+  const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  const hoursOfDay = [
+    "9:00",
+    "10:00",
+    "11:00",
+    "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
+    "16:00",
+    "17:00",
+    "18:00",
+  ];
+
+  return <div className="calendar">
+    <Container>
+        <Row>
+            <Col>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            {daysOfWeek.map((day) => (
+                                <th key={day}>{day}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {hoursOfDay.map((hour) => (
+                            <tr>
+                                <td>{hour}</td>
+                                {daysOfWeek.map((day) => (
+                                    <td key={`${day}-${hour}`}></td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </Col>
+        </Row>
+    </Container>
+                    
+  </div>;
+};
+
 export default Calendar;
